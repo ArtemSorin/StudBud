fca8dceffbd6ea0db4d4185cfa0e82608490f7c2
Artem <artem20072003@outlook.com>
2024-12-22 19:44:21 +0300

    creating navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,301 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _currentIndex = 0;
 
-  void _incrementCounter() {
+  final List<Widget> _screens = [
+    HomeScreen(),
+    LockScreen(),
+    ProfileScreen(),
+    SettingsScreen(),
+  ];
+
+  void _onTabTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentIndex = index;
     });
+  }
+
+  void _addStory() {
+    // Логика добавления истории
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Добавить историю'),
+          content: Text('Здесь будет логика добавления истории.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Закрыть'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          'Лента',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        leading: Icon(Icons.menu, color: Colors.black),
+        actions: [
+          Icon(Icons.notifications, color: Colors.black),
+          SizedBox(width: 16),
+          Icon(Icons.message, color: Colors.black),
+          SizedBox(width: 16),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 10,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            IconButton(
+              icon: Icon(Icons.home, color: Colors.black),
+              onPressed: () => _onTabTapped(0),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            IconButton(
+              icon: Icon(Icons.chat, color: Colors.black),
+              onPressed: () => _onTabTapped(1),
+            ),
+            SizedBox(width: 40), // Пространство для плавающей кнопки
+            IconButton(
+              icon: Icon(Icons.person, color: Colors.black),
+              onPressed: () => _onTabTapped(2),
+            ),
+            IconButton(
+              icon: Icon(Icons.settings, color: Colors.black),
+              onPressed: () => _onTabTapped(3),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _addStory,
+        child: Icon(Icons.add, color: Colors.white),
+        backgroundColor: Colors.black,
+        shape: CircleBorder(),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+
+  Widget _buildStoryAvatar(String name, IconData? icon, {String? imageUrl}) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.black,
+                child: imageUrl != null
+                    ? CircleAvatar(
+                        radius: 28,
+                        backgroundImage: NetworkImage(imageUrl),
+                      )
+                    : Icon(icon, size: 28, color: Colors.white),
+              ),
+              SizedBox(height: 5),
+              Text(name),
+            ],
+          ),
+        ));
+  }
+
+  Widget _buildImagePost(String imageUrl) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8.0),
+      child: Image.network(imageUrl, fit: BoxFit.cover),
+    );
+  }
+}
+
+// Экран главной страницы
+class HomeScreen extends StatelessWidget {
+  Widget _buildImagePost(String imageUrl) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8.0),
+      child: Image.network(imageUrl, fit: BoxFit.cover),
+    );
+  }
+
+  Widget _buildStoryAvatar(String name, IconData? icon, {String? imageUrl}) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: Colors.black,
+            child: imageUrl != null
+                ? CircleAvatar(
+                    radius: 28,
+                    backgroundImage: NetworkImage(imageUrl),
+                  )
+                : Icon(icon, size: 28, color: Colors.white),
+          ),
+          Text(name),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        SizedBox(height: 10),
+        Container(
+          height: 100,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              _buildStoryAvatar('Add Story', Icons.add),
+              _buildStoryAvatar('Anna Asol', null,
+                  imageUrl: 'https://via.placeholder.com/50'),
+              _buildStoryAvatar('Anna Asol', null,
+                  imageUrl: 'https://via.placeholder.com/50'),
+              _buildStoryAvatar('Anna Asol', null,
+                  imageUrl: 'https://via.placeholder.com/50'),
+              _buildStoryAvatar('Anna Asol', null,
+                  imageUrl: 'https://via.placeholder.com/50'),
+            ],
+          ),
+        ),
+        Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage:
+                        NetworkImage('https://via.placeholder.com/50'),
+                    radius: 25,
+                  ),
+                  SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Anna Asol',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Just Now',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Text(
+                "I want to show my TOEFL result. What do you think about it?",
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 10),
+              GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                children: [
+                  _buildImagePost('https://via.placeholder.com/150'),
+                  _buildImagePost('https://via.placeholder.com/150'),
+                  _buildImagePost('https://via.placeholder.com/150'),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.favorite, color: Colors.black),
+                      SizedBox(width: 5),
+                      Text("3.2K"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.comment, color: Colors.black),
+                      SizedBox(width: 5),
+                      Text("333"),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+// Экран чатов
+class LockScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Экран чатов'),
+    );
+  }
+}
+
+// Экран профиля
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Экран профиля'),
+    );
+  }
+}
+
+// Экран настроек
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Экран настроек'),
     );
   }
 }
